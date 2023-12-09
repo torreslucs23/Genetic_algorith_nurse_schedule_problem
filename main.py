bb25467fe1ce9f32d33264dada61af99f524d4ef
RenatoStanz <stanz@tutamail.com>
2023-12-09 05:29:42 -0300

    improvements on main input parameters and adding a gitignore file

--- main.py
+++ main.py
@@ -24,9 +24,7 @@ except Exception as exc:
                     "the file!") from exc
 
 n_individual, k, n = [int(i) for i in first_line]
-#instance = generate_population(21, 10, 21)
 instance = generate_population(n_individual, k, n)
 
-n_mutation, t_crossover,  t_mutation, n_iterations, n_elitism, k, n = [int(i) for i in second_line]
-#50, 0, 0, 100, 25, 10, 21
+n_mutation, t_crossover,  t_mutation, n_iterations, n_elitism = [int(i) for i in second_line]
 res = (genetic(instance, n_mutation, t_crossover,  t_mutation, n_iterations, n_elitism, k, n))
